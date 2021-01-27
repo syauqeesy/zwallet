1fd28d3d75b08037399d7a35a2b80fdc262f8943
syauqeesy <syauqeesy@gmail.com>
2021-01-27 19:28:36 +0700

    Add security PIN confirmation when register

--- src/pages/auth/CreatePIN.vue
+++ src/pages/auth/CreatePIN.vue
@@ -1,34 +1,69 @@
 <template>
   <form class="my-5" @submit.prevent="register">
+    <p class="fs-6">Security PIN</p>
     <div class="row">
       <div class="col-2">
         <div class="mb-3">
-          <input type="text" class="form-control text-center" v-model="firstDigit" :class="{'is-invalid': firstDigitValidity !== 'valid' && firstDigitValidity !== 'pending'}" ref="firstDigit" @keyup="moveToSecondDigit" @blur="validateFirstDigit" autofocus>
+          <input type="password" class="form-control text-center" v-model="firstDigit" autofocus>
         </div>
       </div>
       <div class="col-2">
         <div class="mb-3">
-          <input type="text" class="form-control text-center" v-model="secondDigit" :class="{'is-invalid': secondDigitValidity !== 'valid' && secondDigitValidity !== 'pending'}" ref="secondDigit" @keyup="moveToThirdDigit" @blur="validateSecondDigit">
+          <input type="password" class="form-control text-center" v-model="secondDigit">
         </div>
       </div>
       <div class="col-2">
         <div class="mb-3">
-          <input type="text" class="form-control text-center" v-model="thirdDigit" :class="{'is-invalid': thirdDigitValidity !== 'valid' && thirdDigitValidity !== 'pending'}" ref="thirdDigit" @keyup="moveToForthDigit" @blur="validateThirdDigit">
+          <input type="password" class="form-control text-center" v-model="thirdDigit">
         </div>
       </div>
       <div class="col-2">
         <div class="mb-3">
-          <input type="text" class="form-control text-center" v-model="forthDigit" :class="{'is-invalid': forthDigitValidity !== 'valid' && forthDigitValidity !== 'pending'}" ref="forthDigit" @keyup="moveToFifthDigit" @blur="validateForthDigit">
+          <input type="password" class="form-control text-center" v-model="forthDigit">
         </div>
       </div>
       <div class="col-2">
         <div class="mb-3">
-          <input type="text" class="form-control text-center" v-model="fifthDigit" :class="{'is-invalid': fifthDigitValidity !== 'valid' && fifthDigitValidity !== 'pending'}" ref="fifthDigit" @keyup="moveToSixthDigit" @blur="validateFifthDigit">
+          <input type="password" class="form-control text-center" v-model="fifthDigit">
         </div>
       </div>
       <div class="col-2">
         <div class="mb-3">
-          <input type="text" class="form-control text-center" v-model="sixthDigit" :class="{'is-invalid': sixthDigitValidity !== 'valid' && sixthDigitValidity !== 'pending'}" ref="sixthDigit" @blur="validateSixthDigit">
+          <input type="password" class="form-control text-center" v-model="sixthDigit">
+        </div>
+      </div>
+    </div>
+
+    <p class="fs-6 mt-5">Repeat security PIN</p>
+    <div class="row">
+      <div class="col-2">
+        <div class="mb-3">
+          <input type="password" class="form-control text-center" v-model="firstDigitConfirmation" autofocus>
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="mb-3">
+          <input type="password" class="form-control text-center" v-model="secondDigitConfirmation">
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="mb-3">
+          <input type="password" class="form-control text-center" v-model="thirdDigitConfirmation">
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="mb-3">
+          <input type="password" class="form-control text-center" v-model="forthDigitConfirmation">
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="mb-3">
+          <input type="password" class="form-control text-center" v-model="fifthDigitConfirmation">
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="mb-3">
+          <input type="password" class="form-control text-center" v-model="sixthDigitConfirmation">
         </div>
       </div>
     </div>
@@ -50,17 +85,17 @@ export default {
     return {
       isLoading: false,
       firstDigit: '',
-      firstDigitValidity: 'pending',
       secondDigit: '',
-      secondDigitValidity: 'pending',
       thirdDigit: '',
-      thirdDigitValidity: 'pending',
       forthDigit: '',
-      forthDigitValidity: 'pending',
       fifthDigit: '',
-      fifthDigitValidity: 'pending',
       sixthDigit: '',
-      sixthDigitValidity: 'pending'
+      firstDigitConfirmation: '',
+      secondDigitConfirmation: '',
+      thirdDigitConfirmation: '',
+      forthDigitConfirmation: '',
+      fifthDigitConfirmation: '',
+      sixthDigitConfirmation: ''
     }
   },
   computed: {
@@ -73,82 +108,14 @@ export default {
   },
   methods: {
     ...mapActions(['requestRegister']),
-    validateFirstDigit () {
-      if (this.firstDigit.length < 1 || this.firstDigit.length > 1) {
-        return this.firstDigitValidity = 'invalid'
-      }
-      if (isNaN(parseInt(this.firstDigit))) {
-        return this.firstDigitValidity = 'invalid'
-      }
-      return this.firstDigitValidity = 'valid'
-    },
-    moveToSecondDigit () {
-      this.$refs.firstDigit.blur()
-      this.$refs.secondDigit.focus()
-    },
-    validateSecondDigit () {
-      if (this.secondDigit.length < 1 || this.secondDigit.length > 1) {
-        return this.secondDigitValidity = 'invalid'
-      }
-      if (isNaN(parseInt(this.secondDigit))) {
-        return this.secondDigitValidity = 'invalid'
-      }
-      return this.secondDigitValidity = 'valid'
-    },
-    moveToThirdDigit () {
-      this.$refs.secondDigit.blur()
-      this.$refs.thirdDigit.focus()
-    },
-    validateThirdDigit () {
-      if (this.thirdDigit.length < 1 || this.thirdDigit.length > 1) {
-        return this.thirdDigitValidity = 'invalid'
-      }
-      if (isNaN(parseInt(this.thirdDigit))) {
-        return this.thirdDigitValidity = 'invalid'
-      }
-      return this.thirdDigitValidity = 'valid'
-    },
-    moveToForthDigit () {
-      this.$refs.thirdDigit.blur()
-      this.$refs.forthDigit.focus()
-    },
-    validateForthDigit () {
-      if (this.forthDigit.length < 1 || this.forthDigit.length > 1) {
-        return this.forthDigitValidity = 'invalid'
-      }
-      if (isNaN(parseInt(this.forthDigit))) {
-        return this.forthDigitValidity = 'invalid'
-      }
-      return this.forthDigitValidity = 'valid'
-    },
-    moveToFifthDigit () {
-      this.$refs.forthDigit.blur()
-      this.$refs.fifthDigit.focus()
-    },
-    validateFifthDigit () {
-      if (this.fifthDigit.length < 1 || this.fifthDigit.length > 1) {
-        return this.fifthDigitValidity = 'invalid'
-      }
-      if (isNaN(parseInt(this.fifthDigit))) {
-        return this.fifthDigitValidity = 'invalid'
-      }
-      return this.fifthDigitValidity = 'valid'
-    },
-    moveToSixthDigit () {
-      this.$refs.fifthDigit.blur()
-      this.$refs.sixthDigit.focus()
-    },
-    validateSixthDigit () {
-      if (this.sixthDigit.length < 1 || this.sixthDigit.length > 1) {
-        return this.sixthDigitValidity = 'invalid'
-      }
-      if (isNaN(parseInt(this.sixthDigit))) {
-        return this.sixthDigitValidity = 'invalid'
-      }
-      return this.sixthDigitValidity = 'valid'
-    },
     async register () {
       const securityPIN = this.firstDigit + this.secondDigit + this.thirdDigit + this.forthDigit + this.fifthDigit + this.sixthDigit
+      const securityPINConfirmation = this.firstDigitConfirmation + this.secondDigitConfirmation + this.thirdDigitConfirmation + this.forthDigitConfirmation + this.fifthDigitConfirmation + this.sixthDigitConfirmation
+
+      if (securityPINConfirmation !== securityPIN) {
+        return swal.fire('Failed', 'Security PIN not match!', 'error')
+      }
+
       try {
         this.isLoading = true
         const result = await this.requestRegister({
